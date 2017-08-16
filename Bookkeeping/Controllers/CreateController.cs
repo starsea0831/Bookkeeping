6961b9ebcbb41220b326c267ba357d041377bda7
LuLu <starsea0831@gmail.com>
2017-08-17 00:37:22 +0800

    HW2

--- Bookkeeping/Controllers/CreateController.cs
+++ Bookkeeping/Controllers/CreateController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Bookkeeping.Service;
 
 namespace Bookkeeping.Controllers
 {
@@ -12,14 +13,20 @@ namespace Bookkeeping.Controllers
 		// GET: Create
 		public ActionResult Index()
 		{
+			dbService srv =new dbService();
+			List<AccountBook> AcBook = srv.GetAccountBooks();
 			List<ShowViewModels> shows = new List<ShowViewModels>();
-			shows.Add(new ShowViewModels() { Kind = "支出", Date = new DateTime(2017, 8, 1), Money = 300 });
-			shows.Add(new ShowViewModels() { Kind = "支出", Date = new DateTime(2017, 8, 2), Money = 300 });
-			shows.Add(new ShowViewModels() { Kind = "收入", Date = new DateTime(2017, 8, 3), Money = 500 });
-			shows.Add(new ShowViewModels() { Kind = "支出", Date = new DateTime(2017, 8, 4), Money = 300 });
-			shows.Add(new ShowViewModels() { Kind = "支出", Date = new DateTime(2017, 8, 5), Money = 400 });
-			shows.Add(new ShowViewModels() { Kind = "收入", Date = new DateTime(2017, 8, 6), Money = 300 });
-			shows.Add(new ShowViewModels() { Kind = "支出", Date = new DateTime(2017, 8, 7), Money = 700 });
+			foreach (var accountBook in AcBook)
+			{
+				shows.Add(new ShowViewModels()
+				{
+					Kind = accountBook.Categoryyy == 0 ? "收入" : "支出",
+					Date = accountBook.Dateee,
+					Money = accountBook.Amounttt
+				}
+				);
+			}
+
 			return View(shows);
 		}
 	}
---
+++ Bookkeeping/Service/dbService.cs
@@ -0,0 +1,16 @@
+﻿using Bookkeeping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bookkeeping.Service
+{
+	public class dbService
+	{	
+		public List<AccountBook> GetAccountBooks() {
+			SkillTreeHomeworkEntities1 _dbcontext = new SkillTreeHomeworkEntities1();
+			return _dbcontext.AccountBook.ToList();
+		}
+	}
+}
